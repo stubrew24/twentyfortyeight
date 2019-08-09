7a1f5afc62c9c494435a037eb1d39344a4f2b590
stubrew24 <hello@stuartsewell.dev>
2019-08-09 13:25:55 +0100

    Second MVP

--- src/components/Board.js
+++ src/components/Board.js
@@ -11,26 +11,32 @@ export default class Board extends React.Component{
     ];
 
     state = {
-        values: this.clearBoard
+        values: this.clearBoard,
+        score: 0,
+        moved: false
     };
 
     handleKeyDown = key => {
         switch (key) {
             case 'ArrowDown':
                 this.shiftDown();
-                this.plusTwo();
+                this.checkGameOver()
+                this.setState({moved: false})
                 break;
             case 'ArrowUp':
                 this.shiftUp();
-                this.plusTwo();
+                this.checkGameOver()
+                this.setState({moved: false})
                 break;
             case 'ArrowLeft':
                 this.shiftLeft();
-                this.plusTwo();
+                this.checkGameOver()
+                this.setState({moved: false})
                 break;
             case 'ArrowRight':
                 this.shiftRight();
-                this.plusTwo();
+                this.checkGameOver()
+                this.setState({moved: false})
                 break;
             default:
                 break;
@@ -55,11 +61,31 @@ export default class Board extends React.Component{
             const coords = this.emptySpaces(values)[this.rand(this.emptySpaces(values).length)];
             values[coords[0]][coords[1]] = {value: 2, merged: false};
             this.setState({values})
-        } else {
-            this.gameOver()
         }
     };
 
+    checkGameOver = () => {
+        let moves = false
+        const board = this.state.values
+        if (this.emptySpaces(board).length > 0) moves = true
+        board.forEach((row,rowIndex) => {
+
+            row.forEach((col, colIndex) => {
+                let cur = row[colIndex]
+                let next = row[colIndex + 1]
+
+                if (cur === next) moves = true
+                if (board[rowIndex-1] !== undefined && cur === board[rowIndex-1][colIndex]) moves = true
+                if (cur !== null && next !== null && next !== undefined && board[rowIndex-1] !== undefined && board[rowIndex-1][colIndex] !== null){
+                    console.log(cur)
+                    console.log(next)
+                    if (cur.value === next.value) moves = true
+                    if (cur.value === board[rowIndex-1][colIndex].value) moves = true
+                }
+            })
+        })
+        if (!moves) this.gameOver()
+    };
     rand(num){
         return (Math.random() * num) | 0
     };
@@ -70,7 +96,8 @@ export default class Board extends React.Component{
 
     gameOver = () => {
         alert('Game Over!');
-        this.setState({ values: this.clearBoard })
+        console.log(this.state)
+        this.setState({ values: this.clearBoard, score: 0 })
     };
 
     resetMerged = row => {
@@ -95,17 +122,19 @@ export default class Board extends React.Component{
                     row[currentIndex - 1] = currCell;
                     row[currentIndex] = null;
                     currentIndex -= 1;
+                    this.setState({moved: true})
                     continue
                 }
 
                 if (prevCell === undefined || prevCell.merged || prevCell.value !== currCell.value) break;
 
                 row[currentIndex - 1] = {...prevCell, value: prevCell.value * 2, merged: true};
+                this.setState({score: this.state.score + (prevCell.value * 2)})
+                this.setState({moved: true})
                 row[currentIndex] = null;
                 break
             }
         });
-        return row;
     };
 
     verticalShift = values => {
@@ -124,17 +153,20 @@ export default class Board extends React.Component{
                         values[currentRowIndex - 1][colIndex] = currCell;
                         values[currentRowIndex][colIndex] = null;
                         currentRowIndex -= 1;
+                        this.setState({moved: true})
                         continue
                     }
 
                     if(prevCell === undefined || prevCell.merged || prevCell.value !== currCell.value) break;
 
                     values[currentRowIndex - 1][colIndex] = {...prevCell, value: prevCell.value * 2, merged: true};
+                    this.setState({score: this.state.score + (prevCell.value * 2)})
                     row[colIndex] = null;
+                    this.setState({moved: true})
                     break
                 }
             });
-            return row;
+            // return row;
         });
     };
 
@@ -142,13 +174,18 @@ export default class Board extends React.Component{
         const values = this.state.values;
         this.verticalShift(values)
         values.forEach(row => this.resetMerged(row))
+        this.setState({values})
+        if (this.state.moved) this.plusTwo()
     };
 
     shiftDown = () => {
         const values = this.state.values;
         values.reverse();
-        this.shiftUp();
+        this.verticalShift(values)
+        values.forEach(row => this.resetMerged(row))
         values.reverse();
+        this.setState({values})
+        if (this.state.moved) this.plusTwo()
     };
 
     shiftLeft = () => {
@@ -157,6 +194,8 @@ export default class Board extends React.Component{
             this.horizontalShift(row);
             this.resetMerged(row)
         })
+        if (this.state.moved) this.plusTwo()
+        this.setState({values})
     };
 
     shiftRight = () => {
@@ -168,23 +207,26 @@ export default class Board extends React.Component{
 
             this.resetMerged(row)
         })
+        if (this.state.moved) this.plusTwo()
+        this.setState({values})
     };
 
     componentDidMount() {
         document.addEventListener('keydown', e => this.handleKeyDown(e.key));
         if (!this.state.values.reduce(this.sum, 0) > 0){
             this.plusTwo()
+            this.plusTwo()
         }
     }
 
     render() {
-        const vals = this.state.values;
         return (
             <>
-                <h1 id="title">twentyfortyeight</h1>
+                <h1 id="title">twentyfortyeight (BETA)</h1>
+                <h2 style={{textAlign:'center'}}>Score: {this.state.score}</h2>
                 <div className="game-board">
                     {
-                        vals.map((row, rowIndex) => {
+                        this.state.values.map((row, rowIndex) => {
                             return (
                                 <div key={rowIndex} className={'board-row'}>
                                     {
